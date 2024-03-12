06b7a5983f46c6a05385137e7b44323655120564
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-03-12 19:14:58 +0545

    completed accordion

--- src/components/accordion/Index.jsx
+++ src/components/accordion/Index.jsx
@@ -1,34 +1,64 @@
-import React, {useState} from "react";
-import "./styles.css"
+import React, { useState } from "react";
+import "./styles.css";
 import data from "./data";
 
-
 export default function Index() {
-  const [isShow, setIsShow] = useState(null)
+  const [isShow, setIsShow] = useState(null);
+  const [isEnableMultiSelection, setIsEnableMultiSelecttion] = useState(false);
+  const [multi, setMulti] = useState([]);
+
+  function handleClick(){
+    setIsEnableMultiSelecttion(isEnableMultiSelection => !isEnableMultiSelection)
+    setIsShow(null);
+    setMulti([]);
+  }
 
   return (
     <div className="main">
-      <button className="btn">Enable Multi Selection</button>
-      {data.map(el => (
+      <button className="btn" style={{
+        backgroundColor: isEnableMultiSelection ? "red" : null
+      }} onClick={handleClick}>
+        {isEnableMultiSelection ? "Disable Multi Selection" : "Enable Multi Selection"}
+      </button>
+      {data.map((el) => (
         <Accordion
-        heading={el.question}
-        content={el.answer}
-        key={el.id}
-        isShow={isShow}
-        setIsShow={setIsShow}
-        id={el.id}
-      />
+          heading={el.question}
+          content={el.answer}
+          key={el.id}
+          isShow={isShow}
+          setIsShow={setIsShow}
+          id={el.id}
+          isEnableMultiSelection={isEnableMultiSelection}
+          multi={multi}
+          setMulti={setMulti}
+        />
       ))}
     </div>
   );
 }
 
-function Accordion({ heading, content, isShow, setIsShow, id }) {
-  
+function Accordion({ heading, content, isShow, setIsShow, id, multi, setMulti, isEnableMultiSelection }) {
+  function handleSingle(){
+    setIsShow(id === isShow ? null : id)
+  }
+
+  function handleMulti(){
+    let tempArr = [...multi];
+    const findIndexOfCurrentId = tempArr.indexOf(id);
+
+    if(findIndexOfCurrentId === -1) tempArr.push(id)
+    else tempArr.splice(findIndexOfCurrentId, 1)
+    setMulti(tempArr);
+  }
+
+  console.log(multi)
+
   return (
-    <div className="accordion" onClick={() => setIsShow(id)}>
-      <div><h2>{heading}</h2>
-      {isShow === id && <p>{content}</p>}</div>
+    <div className="accordion" onClick={isEnableMultiSelection ? handleMulti : handleSingle}>
+      <div>
+        <h2>{heading}</h2>
+        {isShow === id || multi.indexOf(id) !== -1 ? <p>{content}</p> : null}
+      </div>
       <button className="btn">+</button>
     </div>
   );
